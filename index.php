7dcea639f5e40ae0dcde8cb3870f187335f9d9f1
mk-kimjh <mk.kimjh@gmail.com>
2017-07-25 16:09:15 +0900

    레이아웃 작성 중...

--- index.php
+++ index.php
@@ -5,24 +5,98 @@
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>KOREA UNIVERSITY THE OFFICE OF INTERNATIONAL AFFAIRS</title>
+	<link href="/assets/fontello/css/fontello.css" rel="stylesheet">
 	<link href="/assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+	<link href="/assets/css/default.css" rel="stylesheet">
 	<!--[if lt IE 9]>
 	  <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
 	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 	<![endif]-->
 </head>
-<body class="application">
+<body>
 
 <header id="header">
 	<div class="container">
 		<div class="row">
-			<div class="col-xs-6 col-sm-3 col-md-6 col-lg-10"><div style="height: 100px; background-color: blue;"></div></div>
+			<div class="col-xs-6 col-lg-3">
+				<h1 class="logo">
+					<a href="/">
+						<img class="visible-xs" src="/assets/images/logo_white_xs.png" alt="KOREA UNIVERSITY Global Services Center">
+						<img class="visible-sm visible-md" src="/assets/images/logo_white_sm.png" alt="KOREA UNIVERSITY Global Services Center">
+						<img class="visible-lg" src="/assets/images/logo_white_lg.png" alt="KOREA UNIVERSITY Global Services Center">
+					</a>
+				</h1>
+			</div>
+			<div class="col-xs-6 hidden-lg">
+				<button class="btn btn-link btn-menu" type="button">
+					<i class="icon-menu"></i>
+					<span class="sr-only">메뉴 열기</span>
+				</button>
+			</div>
+			<div class="col-lg-9 visible-lg">
+				<nav class="nav">
+					<ul>
+						<li>
+							<a href="#">About GSC<span class="bar"></span></a>
+							<div class="nav-content">
+								
+							</div>
+						</li>
+						<li>
+							<a href="#">International Student<span class="bar"></span></a>
+							<div class="nav-content">
+								
+							</div>
+						</li>
+						<li>
+							<a href="#">Exchange/Visiting Program<span class="bar"></span></a>
+							<div class="nav-content">
+								
+							</div>
+						</li>
+						<li>
+							<a href="#">Student Services<span class="bar"></span></a>
+							<div class="nav-content">
+								
+							</div>
+						</li>
+					</ul>
+				</nav>
+			</div>
 		</div>
 	</div>
 </header>
-<main id="content">
+
+<main id="content" style="height: 5000px; background-color: #666;">
+
+	<section id="">
+		
+	</section>
 	
 </main>
+
+<footer id="footer">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-sm-3 col-lg-12">
+				<h2 class="logo">
+					<img class="visible-xs" src="/assets/images/logo_footer_xs.png" alt="KOREA UNIVERSITY Global Services Center">
+					<img class="visible-sm visible-md" src="/assets/images/logo_footer_sm.png" alt="KOREA UNIVERSITY Global Services Center">
+					<img class="visible-lg" src="/assets/images/logo_footer_lg.png" alt="KOREA UNIVERSITY Global Services Center">
+				</h2>
+			</div>
+			<div class="col-xs-12 col-sm-9 col-lg-10">
+				<p class="copyright">
+					Copyright ⓒ 2017 KOREA UNIVERSITY Office of
+					International Affairs. All Rights Reserved.
+				</p>
+			</div>
+			<div class="col-lg-2 visible-lg">
+				<a class="sitemap" href="#">SITE</a>
+			</div>
+		</div>
+	</div>
+</footer>
 
 <script async src="/assets/js/jquery.min.js"></script>
 <script async src="/assets/bootstrap/js/bootstrap.min.js"></script>
