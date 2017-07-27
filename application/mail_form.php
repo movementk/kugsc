6d00df2b8594426b6438537e2a96a3bbdef6a10f
kimyh5392 <mk.kimyh@gmail.com>
2017-07-27 17:35:59 +0900

    mail form 완료

---
+++ application/mail_form.php
@@ -0,0 +1,51 @@
+<?php require_once($_SERVER["DOCUMENT_ROOT"]."/application/_dochead.php"); ?>
+</head>
+
+<body>
+    <div class="kugsc-wrapper" style="font-family: 'Tahoma', sans-serif; background-color: #fff; width: 700px; margin: 0 auto;">
+        <div class="kugsc-header" style="background-color: #8b0029; padding: 8px 20px;">
+            <h1>
+                <img src="/assets/images/logo_white_lg.png" alt="">
+            </h1>
+        </div>
+        <div class="kugsc-content" style="width: 570px; margin: 0 auto; text-align: center; margin-top: 90px;">
+            <h3 style="font-size: 33px; line-height: 50px; font-weight: 400; color: #111;">
+                <span style="display: inline; color: #910023; border-bottom: 2px solid #910023; padding-right: 5px;">OOO University</span><span style="display: inline; border-bottom: 2px solid #111; padding-left: 5px;">representative</span><br>
+                <span style="display: inline; color: #910023; border-bottom: 2px solid #910023; padding-right: 5px;">of OOO</span><span style="display: inline; border-bottom: 2px solid #111; padding-left: 5px;">recommended you.</span>
+            </h3>
+            <div class="kugsc-mail-content" style="margin-top: 60px; padding: 45px 85px; background-color: #f5f5f5; text-align: left;">
+                <p style="font-size: 14px; color: #555; line-height: 25px;">
+                    Sample text Sample text Sample text Sample text Sample text Sample text Sample text Sample text Sample text Sample text Sample text Sample text Sample text Sample text Sample text Sample text Sample text Sample text Sample text Sample text 
+                </p>
+            </div>
+            <div class="kugsc-btn-area" style="margin-top: 50px; text-align: center; padding-bottom: 70px;">
+                <p style="font-size: 15px; color: #333; margin-bottom: 50px;">Click on the buttons below to complete your application.</p>
+                <p style="width: 160px; margin: 0 auto;">
+                    <a href="#" class="kugsc-btn" role="button" style="display: block; padding: 12px 0; background-color: #8b0029; color: #fff; text-align: center; font-size: 15px; text-decoration: none;">
+                        <img src="/assets/images/application/apply_btn_bg.png" alt="" style="display: inlnie-block; vertical-align: middle; margin-top: -2px; margin-right: 3px;">
+                        APPLY
+                    </a>
+                </p>
+            </div>
+        </div>
+        
+        <div class="kugsc-footer" style="background-color: #e9e9e9; padding: 35px 0 27px 20px; border-top: 1px solid #ccc; background-color: #e9e9e9;">
+            <h2 style="margin-bottom: 10px;">
+                <img src="/assets/images/application/mail_foot_logo.png" alt="">
+            </h2>
+            <p class="kugsc-addr" style="font-size: 11px; color: #666; line-height: 18px;">
+                Central Plaza B1 #104A, 145 Anam-Ro, Seongbuk-Gu, Seoul 02841, Korea (near KU Pride Club)
+            </p>
+            <ul style="color: #666; font-size: 0; margin: 3px 0;">
+                <li style="font-size: 11px; display: inline-block; line-height: 9px; padding-right: 7px; border-right: 1px solid #666;">Tel : +82-2-3290-5173, 5174</li>
+                <li style="font-size: 11px; display: inline-block; line-height: 9px; padding: 0 7px; border-left: 1px solid #cbcbcb; border-right: 1px solid #666;">Email : gsc@korea.ac.kr</li>
+                <li style="font-size: 11px; display: inline-block; line-height: 9px; padding-left: 7px; border-left: 1px solid #cbcbcb;">Fax : +82-921-2352</li>
+            </ul>
+            <p class="kugsc-copyright" style="font-size: 11px; color: #666; line-height: 18px;">
+                (C) KOREA UNIVERSITY Office of International Affairs. All Rights Reserved.
+            </p>
+        </div>
+    </div>
+</body>
+
+</html>
