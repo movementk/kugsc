b60e679dec11409126f7fac73115f7a25c65ff14
mk-kimjh <mk.kimjh@gmail.com>
2017-07-28 18:28:37 +0900

    가이드 완료(탭 부분 제외)

--- sub.php
+++ sub.php
@@ -57,59 +57,111 @@
 						</div>
 					</div>
 					
+					<br><br>##### 제목 #####<br><br>
+					
 					<h4 class="title">What is an ‘Orientation Week’?</h4>
 					
-					<p class="u-title">What is an ‘Orientation Week’?</p>
+					<h5 class="u-title">What is an ‘Orientation Week’?</h5>
+					
+					<h5 class="c-title"><i>C</i> Information on Necessary Documents for Claims</h5>
+					
+					<br><br>##### 문단 #####<br><br>
 					
 					<p>Orientation Week offers you social, sporting, study and support sessions and events to help you get familiar with the campus, meet people, get prepared for study, find out who’s there to help and most importantly have fun!</p>
 
 					<p>Attending orientation is an important step of starting university so attendance is considered essential. The orientation conducted by Global Services Center is mandatory.</p>
 					
+					<br><br>##### 리스트 #####<br><br>
+					<!-- 순서 있는 리스트(원, 번호) -->
 					<ol class="co-list">
 						<li>
 							Getting to KU – Airport Pickup Service
-							<p class="dot">Pay your tuition and other optional fees, especially mutual health aid association fee</p>
+							<p>If you apply or make a payment after the
+application period, individual rate would be
+applied which will result in higher cost of
+insurance.</p>
 						</li>
 						<li>
 							Find <a href="#">Accommodation</a>
-							<p class="link">Pay your tuition and other optional fees, especially mutual health aid association fee</p>
 						</li>
 						<li>
 							Getting to KU – Airport Pickup Service
-							<p>Pay your tuition and other optional fees, especially mutual health aid association fee</p>
 						</li>
 					</ol>
 					
-					<ol class="u-list">
+					<!-- 순서 없는 리스트(점) -->
+					<ul class="u-list">
 						<li>
-							Getting to KU – Airport Pickup Service
-							<p class="dot">Pay your tuition and other optional fees, especially mutual health aid association fee</p>
+							KEB Hana Bank - Godae Branch (Swift Code: KOEXKRSE) 145 Anam-Ro, Seongbuk-Gu, Seoul, Korea 02841 
 						</li>
 						<li>
 							Find <a href="#">Accommodation</a>
-							<p class="link">Pay your tuition and other optional fees, especially mutual health aid association fee</p>
 						</li>
 						<li>
 							Getting to KU – Airport Pickup Service
-							<p>Pay your tuition and other optional fees, especially mutual health aid association fee</p>
 						</li>
-					</ol>
+					</ul>
 					
-					<div class="box">
-						<ol class="co-list">
-							<li>
-								Getting to KU – Airport Pickup Service
-								<p class="dot">Pay your tuition and other optional fees, especially mutual health aid association fee</p>
-							</li>
-							<li>
-								Find <a href="#">Accommodation</a>
-							</li>
-							<li>
-								Getting to KU – Airport Pickup Service
-								<p>Pay your tuition and other optional fees, especially mutual health aid association fee</p>
-							</li>
-						</ol>
+					<br><br>##### 테이블 #####<br><br>
+					
+					<!-- 테이블 -->
+					<div class="table-responsive">
+						<table class="table table-bordered">
+							<thead>
+								<tr>
+									<th rowspan="2" colspan="2">Division</th>
+									<th>Documents</th>
+									<th>Issuer</th>
+								</tr>
+								<tr>
+									<th>Documents</th>
+									<th>Issuer</th>
+								</tr>
+							</thead>
+							<tbody>
+								<tr>
+									<td rowspan="6">Medical Cost</td>
+									<td rowspan="2">Hospitalization (Including surgery)</td>
+									<td>Certificate of discharge (Including diagnosis)</td>
+									<td>Care hospital</td>
+								</tr>
+								<tr>
+									<td>Receipt of treatment</td>
+									<td>Care hospital</td>
+								</tr>
+								<tr>
+									<td rowspan="2">Outpatient Treatment</td>
+									<td>Medical records or First visit medical records</td>
+									<td>Care hospital</td>
+								</tr>
+								<tr>
+									<td>Receipt of treatment</td>
+									<td>Receipt of treatment</td>
+								</tr>
+								<tr>
+									<td rowspan="2">Outpatient Treatment</td>
+									<td>Medical records or First visit medical records</td>
+									<td>Care hospital</td>
+								</tr>
+								<tr>
+									<td>Receipt of treatment</td>
+									<td>Receipt of treatment</td>
+								</tr>
+							</tbody>
+						</table>
 					</div>
+					
+					<br><br>##### 주의사항 문구 #####<br><br>
+					
+					<p class="note">For more information about homepage user guide and claims, download the following:</p>
+					<p class="note"><i>For more information about homepage user guide and claims, download the following:</i></p>
+					
+					
+					<br><br>##### 버튼 #####<br><br>
+					
+					<!-- 버튼 -->
+					<button type="button" class="btn btn-default">Book one-on-one appointment</button>
+					<button type="button" class="btn btn-primary">Book one-on-one appointment</button>
 					
 				</div>
 				
