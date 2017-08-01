97115e24a3157eb1feb3c5c5cced6c9aec5504a8
mk-kimjh <mk.kimjh@gmail.com>
2017-08-01 14:40:57 +0900

    레이아웃: sm 컨테이너 넓이 수정

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/dochead.php"); ?>
+<link href="/assets/css/main.css" rel="stylesheet">
 </head>
 <body>
 
@@ -7,6 +8,23 @@
 
 <main id="content">
 	
+	<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+		<!-- Indicators -->
+		<ol class="carousel-indicators">
+			<li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+			<li data-target="#carousel-example-generic" data-slide-to="1"></li>
+		</ol>
+		<!-- Wrapper for slides -->
+		<div class="carousel-inner" role="listbox">
+			<div class="item active"> <img src="..." alt="...">
+				<div class="carousel-caption"> ... </div>
+			</div>
+			<div class="item"> <img src="..." alt="...">
+				<div class="carousel-caption"> ... </div>
+			</div>
+		</div>
+	</div>
+
 </main>
 
 <?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/footer.php"); ?>
