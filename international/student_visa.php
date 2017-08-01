1632ac24d3241cba4f4ca95eab929ccf1801ed05
kimyh5392 <mk.kimyh@gmail.com>
2017-08-01 10:23:01 +0900

    student_visa 완료.

---
+++ international/student_visa.php
@@ -0,0 +1,76 @@
+<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/dochead_sub.php"); ?>
+<link href="/assets/css/international.css" rel="stylesheet">
+</head>
+<body class="international">
+
+<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/header.php"); ?>
+<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/side_nav.php"); ?>
+
+<main id="content" class="student-visa">
+
+	<!-- Page header -->
+	<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/page_header.php"); ?>
+	<!-- // Page header -->
+	
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-3">
+				<!-- LNB -->
+				<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/lnb.php"); ?>
+				<!-- // LNB -->
+			</div>
+			<div class="col-lg-9">
+				<div class="page-content">
+					<!-- 페이지 타이틀 -->
+					<div class="page-title">
+						<h3>Student VISA</h3>
+					</div>
+                    <p class="summary">
+                        All students, including freshmen are required to have a visa from Korean embassy or consulate office in order to enter Korea. The D-2 visa application will require, among other things, a Certificate of Admission from Korea University and documents showing your financial support (bank statement with letter of financial support from parents or other sponsors if applicable). If you submit certificate of admission to an embassy or a consulate office, they may give D-2 visa without looking at other documents or they may ask for additional documents since it is sole right of embassies to give visa. It is advised that students must contact embassy whether they require any additional documents prior to their application for D-2 visa.<br><br>
+                        Applying for a student visa can be a complicated process. Students who are already in Korea can apply directly at Sejongno immigration office while those from overseas must visit the nearest Korean Embassy or consulate office. Students who wish to apply through Sejongo immigration must make online reservation at <a href="http://hikorea.go.kr" target="_blank">http://hikorea.go.kr</a> one month prior to the visit.
+                    </p>
+                    <div class="required-documents">
+                        <h4 class="u-title">The required documents for D-2 visa at Sejongno immigration office</h4>
+                        <ol class="co-list">
+                            <li>Receipt of the online reservation &amp; Application form</li>
+                            <li>Certificate of Admission (issued by the International Admissions Team)</li>
+                            <li>Passport (original and copy)</li>
+                            <li>Alien Registration Card (only those whohave)</li>
+                            <li>Passport sized Photo (3.5cmX4.5cm with white background which was taken within the past 6 months)</li>
+                            <li>Tuition Payment Receipt</li>
+                            <li>Fee 130,000 KRW only in cash</li>
+                            <li>
+                                Evidence of the place of residence
+                                <ul class="dot-list">
+                                    <li>
+                                        For students living on-campus require a receipt of dormitory fee payment. If you are a freshmen (transfer student) you must submit ‘Confirmation of Residence’ issued by the CJ I-House office.
+                                    </li>
+                                    <li>
+                                        For students living off-campus require one of three from the followings; 1) Receipt of public service fee 2)Advance notice on expiration of your stay 3)Rental contract under your name
+                                    </li>
+                                </ul>
+                            </li>
+                            <li>
+                                Certificate of Requirement for Tuberculosis (TB) test (Only if you are from the high TB-burden countries.) [Tuberculosis Risk Countries*]: Bangladesh, Cambodia, China, East Timor, India, Indonesia, Krgyzstan, Malaysia, Mongolia, Myanmar, Nepal, Pakistan, Russia, Sri Lanka, Thailand, the Philippines, Uzbekistan, Vietnam, Laos
+                                <p>If you did not submit TB test results to immigration office after 2016.03.02</p>
+                                <i>
+                                    TB test can be done at the nearest community medical center. The one close to the campus is <span>Seongbuk Community Health Center(성북구보건소)</span>. Students must bring their passport-sized photo and the fee for TB test is 1,500KRW.
+                                </i>
+                            </li>
+                            <li>
+                                Do not forget to scan your fingerprint before you leave the Immigration Office.
+                            </li>
+                        </ol>
+                    </div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+</main>
+
+<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/footer.php"); ?>
+<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/docfoot_sub.php"); ?>
+
+</body>
+</html>
