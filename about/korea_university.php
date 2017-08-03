ed4a9b2cd46de903aa8964906c8553be11538043
mk-kimjh <mk.kimjh@gmail.com>
2017-08-03 19:21:31 +0900

    about korea university 작성 중...(모바일 버전 완료)

---
+++ about/korea_university.php
@@ -0,0 +1,127 @@
+<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/dochead_sub.php"); ?>
+<link href="/assets/css/about.css" rel="stylesheet">
+</head>
+<body class="about">
+
+<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/header.php"); ?>
+<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/side_nav.php"); ?>
+
+<main id="content" class="korea-univ">
+
+	<!-- Page header -->
+	<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/page_header.php"); ?>
+	<!-- // Page header -->
+	
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-3">
+			
+				<!-- LNB -->
+				<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/lnb_about.php"); ?>
+				<!-- // LNB -->
+				
+			</div>
+			<div class="col-lg-9">
+				
+				<div class="page-content">
+				
+					<!-- 페이지 타이틀 -->
+					<div class="page-title">
+						<h3>About Korea University</h3>
+					</div>
+                   
+                  	<p>
+                  		Korea University, founded in 1905, is widely acknowledged as one of the country’s oldest, largest and the most prestigious universities in Korea. Its reputation for quality is based on excellence in teaching, research and service to Korean society. Eminent faculty, outstanding students and internationally recognized research programs all contribute to Korea University’s academic fame. As a major comprehensive university, KU offers a rich variety of courses in liberal arts and sciences as well as in all professional fields, including law, medicine and business administration.
+					</p>
+					
+					<p>
+						In accordance with KU’s vision, ‘Opening the Future, Leading the Future, Creating the Future’ and KU’s education goal, ‘To encourage the holistic growth of intelligent, spiritual and active potential, nurture capabilities for creative academic study and professional practice, and foster open-minded leadership contributing to the international community’, KU strives to take the lead in creating a new future by developing knowledge that can change and lead our society and the world.
+					</p>
+					
+					<p>
+						Over the past several years, we have seen a dramatic increase in both the number of courses offered in English and in the number of international students who have chosen Korea University as their study abroad destination. In the fall of 2016, we welcomed almost 18,700 students to our campus from all parts of the globe.
+                  	</p>
+                  	
+                  	<h5 class="u-title">Here are some of the key facts on Korea University:</h5>
+                  	
+                  	<ul class="row facts">
+                  		<li class="col-xs-12 col-sm-6">
+                  			<div class="fact-item fact-item-1">
+								<i><img class="img-responsive" src="/assets/images/about/ku/ico_badge_rank_100.png" alt="100"></i>
+								<p>
+									First Private University in Korea
+									to enter the top100
+									QS World Ranking 2017-18
+								</p>
+                  			</div>
+                  		</li>
+                  		<li class="col-xs-12 col-sm-6">
+                  			<div class="fact-item fact-item-2">
+								<i><img class="img-responsive" src="/assets/images/about/ku/ico_badge_rank_16.png" alt="16"></i>
+								<p>
+									#1 private University in
+									Korea and #16 in QS Asian
+									University Rankings 2016
+								</p>
+                  			</div>
+                  		</li>
+                  	</ul>
+                  	
+                  	<figure class="rank-graph">
+                  		<p>
+                  			<img class="img-responsive visible-xs" src="/assets/images/about/ku/img_rank_graph_xs.gif">
+                  			<img class="img-responsive visible-sm visible-md" src="/assets/images/about/ku/img_rank_graph_sm.gif">
+                  			<img class="img-responsive visible-lg" src="/assets/images/about/ku/img_rank_graph_lg.gif">
+                  		</p>
+                  		<figcaption>
+							Rankings of Korea University in the QS<br>
+							World University Rankings<br>
+							for the past 5 years
+                 		</figcaption>
+                  	</figure>
+                  	
+                  	<ul class="row stats">
+                  		<li class="col-xs-12 col-sm-6">
+                  			<div class="stats-item stats-item-1">
+                  				<i><img class="img-responsive" src="/assets/images/about/ku/ico_graduate.png" alt="100"></i>
+                  				<dl>
+                  					<dt>Number of Students (both undergraduate and graduate)</dt>
+                  					<dd>35,620</dd>
+                  				</dl>
+                  			</div>
+                  		</li>
+                  		<li class="col-xs-12 col-sm-6">
+                  			<div class="stats-item stats-item-2">
+                  				<i><img class="img-responsive" src="/assets/images/about/ku/ico_university.png" alt="16"></i>
+                  				<dl>
+                  					<dt>#1 private University in
+									Korea and #16 in QS Asian
+									University Rankings 2016</dt>
+                 					<dd>14,489</dd>
+                  				</dl>
+                  			</div>
+                  		</li>
+                  	</ul>
+                  	
+                  	<div class="stats-graph">
+                  		<h5>International Students by Academic Level</h5>
+                  		<p>
+                  			<img class="img-responsive visible-xs" src="/assets/images/about/ku/img_percent_xs.gif">
+                  			<img class="img-responsive visible-sm visible-md" src="/assets/images/about/ku/img_percent_sm.gif">
+                  			<img class="img-responsive visible-lg" src="/assets/images/about/ku/img_percent_lg.gif">
+                  		</p>
+                  	</div>
+                    
+				</div>
+				
+			</div>
+		</div>
+	</div>
+	
+</main>
+
+<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/footer.php"); ?>
+<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/docfoot_sub.php"); ?>
+
+</body>
+</html>
---
+++ inc/lnb_about.php
@@ -0,0 +1,20 @@
+<nav id="lnb" class="dropdown">
+	<h2 class="visible-lg">About</h2>
+	<button class="btn btn-link btn-block dropdown-toggle" type="button" data-toggle="dropdown">
+		Select
+	</button>
+	<ul class="dropdown-menu">
+		<li>
+			<a href="/about/korea_university.php">About Korea University</a>
+		</li>
+		<li>
+			<a href="/about/about_us.php">About Us</a>
+		</li>
+		<li>
+			<a href="/about/staff_list.php">Staff List</a>
+		</li>
+		<li>
+			<a href="/about/contact_us.php">Contact Us</a>
+		</li>
+	</ul>
+</nav>
