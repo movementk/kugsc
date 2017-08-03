e3ef8f2f8dc3e65002a1809ab941088b7110094e
kimyh5392 <mk.kimyh@gmail.com>
2017-08-03 18:47:39 +0900

    international > insurance.php 작업중 . . .

---
+++ international/part_time.php
@@ -0,0 +1,55 @@
+<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/dochead_sub.php"); ?>
+<link href="/assets/css/international.css" rel="stylesheet">
+</head>
+<body class="international">
+
+<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/header.php"); ?>
+<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/side_nav.php"); ?>
+
+<main id="content" class="part-time">
+
+	<!-- Page header -->
+	<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/page_header.php"); ?>
+	<!-- // Page header -->
+	
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-3">
+				<!-- LNB -->
+				<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/lnb.php"); ?>
+				<!-- // LNB -->
+			</div>
+			<div class="col-lg-9">
+				<div class="page-content">
+					<!-- 페이지 타이틀 -->
+					<div class="page-title">
+						<h3>Work Part-time (D-2)</h3>
+					</div>
+					<p>
+					    It is illegal for D-2 visa holders to obtain work outside of the university and earn an income without prior permission from the immigration. Student must fill in Part-time work confirmation form together with other supporting documents specified below. Once part-time work permission is given, Undergraduate student can work up to 25 hours per week and graduate students can work up to 30 hours per week. No number of working hour is specified for summer and winter vacations.
+					</p>
+					<div class="required-documents">
+					    <h4 class="title">Required Documents</h4>
+					    <ol class="co-list">
+					        <li>
+					            Part-time work of Foreign Student Confirmation Form (Approved by Global Services Center)
+					        </li>
+					        <li>
+					            Copy of a certificate of business registration of your employee
+					        </li>
+					        <li>Certificate of Enrollment</li>
+					        <li>Academic Transcript (min. GPA of 2.0)</li>
+					    </ol>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+</main>
+
+<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/footer.php"); ?>
+<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/docfoot_sub.php"); ?>
+
+</body>
+</html>
--- international/student_orientation.php
+++ international/student_orientation.php
@@ -42,6 +42,125 @@
                     <p>
 					    Please check the schedule and places indicated below and make a note so that you do not miss out on our important sessions for you to prepare your start at Korea University.
 					</p>
+					<div class="collapse-list">
+                        <ul>
+                            <li>
+                                <div class="collapse-content">
+                                    <a class="btn btn-collapse" role="button" data-toggle="collapse" href="#Orientation-1" aria-expanded="false" aria-controls="Orientation-1">
+                                        Prepare before Orientation Week
+                                    </a>
+                                    <div class="collapse in" id="Orientation-1">
+                                        <div class="well">
+                                            <ol class="co-list">
+                                                <li>
+                                                    Enrolling
+                                                    <p class="dott">
+                                                        Pay your tuition and other optional fees, especially mutual health aid association fee
+                                                    </p>
+                                                </li>
+                                                <li>Find <i>Accommodation</i></li>
+                                                <li>Getting to KU – <i>Airport Pickup Service</i></li>
+                                            </ol>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="collapse-content">
+                                    <a class="btn btn-collapse" role="button" data-toggle="collapse" href="#Orientation-2" aria-expanded="false" aria-controls="Orientation-2">
+                                        Orientation Week
+                                    </a>
+                                    <div class="collapse in" id="Orientation-2">
+                                        <div class="well">
+                                            <ol class="co-list">
+                                                <li>
+                                                    Registration
+                                                    <p class="dott">
+                                                        On arrival, student must report to Global Services center his/her updated contact information or copy of Alien<br class="hidden-xs"> Registration Card in the <i>‘Arrival Report’</i>
+                                                    </p>
+                                                </li>
+                                                <li>GSC Orientation Session</li>
+                                                <li>Course Registration &amp; IT session</li>
+                                                <li>KU Campus Tour</li>
+                                                <li>KU Library Tour</li>
+                                                <li>
+                                                    Late Arrival One-on-One Orientation<br class="visible-xs">
+                                                    (Must make an appointment to GSC)
+                                                </li>
+                                            </ol>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="collapse-content">
+                                    <a class="btn btn-collapse" role="button" data-toggle="collapse" href="#Orientation-3" aria-expanded="false" aria-controls="Orientation-3">
+                                        Your first few weeks
+                                    </a>
+                                    <div class="collapse in" id="Orientation-3">
+                                        <div class="well">
+                                            <ol class="co-list">
+                                                <li>Finalize your timetable</li>
+                                                <li>Make connections – <i>KUISA</i></li>
+                                                <li>
+                                                    Develop your academic and personal skills 
+                                                    <a href="#">Free pre-calculus class during a semester</a>
+                                                    <a href="#">CTL one-on-one coaching</a>
+                                                    <a href="#">Free Korean language class during summer/winter</a>
+                                                </li>
+                                            </ol>
+                                        </div>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="orientation-inquires">
+                        <h5 class="u-title">Orientation inquires</h5>
+                        <dl>
+                            <dt>Tel</dt>
+                            <dd>3290-5171~5174,5179</dd>
+                        </dl>
+                    </div>
+                    <h4 class="title">Things to note</h4>
+                    <h5 class="u-title">Student ID Card</h5>
+                    <p>
+                        Students receive student ID card during a specified period of time at the beginning of each semester.
+                    </p>
+                    <p>
+                        To gain access to most of Korea University facilities, you must show your KU photo ID card. There are two types of ID card- one general photo ID or the other one with financial function on it ( SMART ID card). The application period of each semester will be put up on Announcements
+                    </p>
+                    <p>
+                        In order to receive <i class="co-r">general student ID card</i> (no financial function), student must either,
+                    </p>
+                    <p>
+                        <i>Option1)</i> apply online and pick-up from your department office. (NOT Global Services Center)
+                    </p>
+                    <p>
+                        <i>Option2)</i> visit the ONE-STOP service center with a passport-sized photo and apply. (those who have not applied online)
+                    </p>
+                    <p>
+                        For <i class="co-r">smart student ID card</i> with financial function (Debit card), you need to visit your department office to register your Alien Registration number first and then visit Hana Bank on campus with Alien Registration card and apply. 
+                    </p>
+                    <p>
+                        Replacement cards (both general and SMART) for students:  If you have lost your general ID card, you may obtain a replacement card at ONE-STOP service center. You will be charged 5,000KRW for the replacement. As for SMART ID card, once you obtain the replacement card you need to take the card to WASAC BASAC(Hana bank branch) in central plaza to add financial function.
+                    </p>
+                    <div class="kakao-plus">
+                        <h5 class="u-title">KaKao Plus ID</h5>
+                        <p>
+                            The Global Services Center has created KaKao Plus ID to communicate with international student through a Kakao Talk. Once student adds GSC as a ‘Plus friend’, he/she can connect and chat with us one-on-one. Our international students can also receive announcements, events and answers to their queries through this SNS. Please add KUGSC as a friend by
+                        </p>
+                        <ol class="co-list">
+                            <li>clicking the @KUGSC in the following page</li>
+                            <li>scan the QR code right at the bottom of the page.</li>
+                        </ol>
+                        <p>This service is only available in mobile Kakao App.</p>
+                        <figure>
+                            <img src="/assets/images/international/img_kakao_xs.jpg" class="img-responsive visible-xs" alt="kakao plus @KUGSC">
+                            <img src="/assets/images/international/img_kakao_sm.jpg" class="img-responsive visible-sm" alt="kakao plus @KUGSC">
+                            <img src="/assets/images/international/img_kakao_lg.jpg" class="img-responsive visible-md visible-lg" alt="kakao plus @KUGSC">
+                        </figure>
+                    </div>
 				</div>
 			</div>
 		</div>
