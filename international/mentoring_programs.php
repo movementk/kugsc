25917363414fb41e4274d71381603e2f5142ce9b
kimyh5392 <mk.kimyh@gmail.com>
2017-08-04 16:12:49 +0900

    Commit #2

---
+++ international/mentoring_programs.php
@@ -0,0 +1,159 @@
+<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/dochead_sub.php"); ?>
+<link href="/assets/css/international.css" rel="stylesheet">
+</head>
+<body class="international">
+
+<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/header.php"); ?>
+<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/side_nav.php"); ?>
+
+<main id="content" class="mentoring-programs">
+
+	<!-- Page header -->
+	<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/page_header.php"); ?>
+	<!-- // Page header -->
+	
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-3">
+				<!-- LNB -->
+				<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/lnb_international.php"); ?>
+				<!-- // LNB -->
+			</div>
+			<div class="col-lg-9">
+				<div class="page-content">
+					<!-- 페이지 타이틀 -->
+					<div class="page-title">
+						<h3>Mentoring Programs</h3>
+					</div>
+                    <div class="tabs dropdown">
+						<button class="btn btn-defualt btn-block" data-toggle="dropdown">
+							KUISA
+						</button>
+						<div class="dropdown-menu">
+							<ul class="nav nav-tabs nav-justified" role="tablist">
+								<li role="presentation" class="active"><a href="#mentoring-1" aria-controls="mentoring-1" role="tab" data-toggle="tab">KUISA</a></li>
+								<li role="presentation"><a href="#mentoring-2" aria-controls="mentoring-2" role="tab" data-toggle="tab">Free classes</a></li>
+								<li role="presentation"><a href="#mentoring-3" aria-controls="mentoring-3" role="tab" data-toggle="tab">Language Exchange</a></li>
+							</ul>
+						</div>
+					</div>
+
+					<!-- 탭 패널 -->
+					<div class="tab-content">
+						<div role="tabpanel" class="tab-pane active" id="mentoring-1">
+							<h4 class="title">KUISA (Student Buddy Program)</h4>
+							<div class="row">
+							    <div class="col-xs-12 col-sm-4 col-md-3">
+							        <figure>
+							            <img src="/assets/images/international/mentoring_xs_img01.jpg" class="img-responsive visible-xs" alt="">
+							            <img src="/assets/images/international/mentoring_sm_img01.jpg" class="img-responsive hidden-xs" alt="">
+							        </figure>
+							    </div>
+							    <div class="col-xs-12 col-sm-8 col-md-9">
+							        <p>
+							            KUISA, also known as Korea University International Student Assistant, is a voluntary student organization under Global Services Center. Since its foundation in 2013, KUISA has been successfully assisting and helping international students to adjust well to Korea University as well as Korea.KUISA aims to guide in university life and make friends with KU students who came from all walks of life and different cultural backgrounds. 
+							        </p>
+							    </div>
+							</div>
+							<h5 class="u-title">How it works</h5>
+							<p>
+							    The bud and buddy help each other in academic related matters and other difficulties faced in everyday life. Not only do they promote friendship and cultures through various events, language exchange and others but this is also an opportunity for them to nurture on another. 
+							</p>
+							<div class="row">
+							    <div class="col-xs-12 col-sm-4 space">
+                                    <p class="justify">
+                                        The <b>[BUD]</b> is Korea University Korean students who can help new international students. KUISA recruits BUD each semester by interview and screening his/her application. BUD usually serves for one semester but if a BUD wishes to continue, he/she can do so.
+                                    </p>
+							    </div>
+							    <div class="col-xs-12 col-sm-4 space">
+							        <figure>
+							            <img src="/assets/images/international/mentoring_xs_img02.jpg" class="img-responsive visible-xs" alt="">
+							            <img src="/assets/images/international/mentoring_sm_img02.jpg" class="img-responsive hidden-xs" alt="">
+							        </figure>
+							    </div>
+							    <div class="col-xs-12 col-sm-4 space">
+                                    <p class="justify">
+                                        The <b>[BUDDY]</b> is new international or overseas Korean students who receive help from the KUISA BUDs. There is no recruitment process for buddies. All KU international and overseas Korean students are able to join activities with KUISA.                                        
+                                    </p>
+							    </div>
+							</div>
+							<div class="box">
+							    <ol class="co-list">
+							        <li>1:1 Course registration session : Newly joined international students unfamiliar with the registration system are assisted by KUISA on the day of registration at on-campus computer lab</li>
+							        <li>Regular get-together : once a week</li>
+							        <li>Correction day : correction of assignments written in Korean</li>
+							        <li>Meeting with seniors : topics include major, internship, exchange program and others</li>
+							        <li>lub activities : Watching sports games, Playing bowling, Exploring popular restaurants and so on</li>
+							        <li>Field Day : Sport activities and games among KUISA members and  nternational students at KU Nokjee sports field</li>
+							        <li>Students’ festivals, cultural events and going for excursions</li>
+							    </ol>
+							    <div class="btn-area">
+							        <button type="button" class="btn btn-primary"><i class="icon-doc"></i> Join KUISA</button>
+							    </div>
+							</div>
+                            <div class="embed-responsive embed-responsive-16by9 video">
+                                <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/yaukIOb9cjU"></iframe>
+                            </div>
+						</div>
+						<div role="tabpanel" class="tab-pane" id="mentoring-2">
+							<p>
+							    Korea University puts best effort to help you get the most out of your studies, tackle assignments and also prepare well for exams. Our free classes can help you improve your fundamental knowledge about the subject, develop skills and give you detailed feedback on assignments.
+							</p>
+							<h4 class="title">Free Mathematics(Pre-calculus) Class</h4>
+							<p>
+							    Free Mathematics class is for students who wish to improve fundamental knowledge about calculus. It is an intensive course for 10 weeks, classes would be held twice a week, and students are free to register.
+							</p>
+							<h5 class="u-title">When and How to apply</h5>
+							<ol class="co-list">
+							    <li>Spring semester – March</li>
+							    <li>Fall semester – September</li>
+							</ol>
+							<p class="note">
+							    <i>
+							        The detailed information will be available at 'Announcements'.
+							    </i>
+							</p>
+							<h4 class="title">Free Korean Language Class for Summer/Winter vacation:</h4>
+							<p>
+							    Free Korean Language class is held to facilitate students to get more involved and get used to Korean language including vocabulary, grammar and daily conversation. It would be a great opportunity to learn more about Korea, become accustomed to Korean culture and make more international friends on campus. It is an intensive 4-week course and classes will be held from Monday to Thursday every week.
+							</p>
+							<h5 class="u-title">When and How to apply</h5>
+							<ol class="co-list">
+							    <li>Spring semester – in June</li>
+							    <li>Fall semester – in December</li>
+							</ol>
+							<p class="note">
+							    <i>
+							        The detailed information will be available at 'Announcements'.
+							    </i>
+							</p>
+						</div>
+						<div role="tabpanel" class="tab-pane" id="mentoring-3">
+							<h4 class="title">Language Exchange Program</h4>
+							<p>
+							    The Global Services Center is offering language exchange program during the semester for students who wish to boost their Korean language as well as teach local students their languages and culture.<br>
+							    A language exchange involves two speakers of different languages who practice these languages together. For example, a student who is fluent in Chinese may want to practice her Korean. She will then be paired with KU local student who also wants to practice Chinese. In this way, both students learn and assist. Language exchange partners have the independence to structure their meetings according to their needs.<br>
+							    This is a one-on-one program where the center matches students based on their study plan and language preferences. Please be advised that this is a voluntary program.
+							</p>
+							<h5 class="u-title">When and How to apply</h5>
+							<ol class="co-list">
+							    <li>Spring semester – in April</li>
+							    <li>Fall semester – in October</li>
+							</ol>
+							<p class="note">
+							    <i>For more information, please check 'Announcements' in April and October.</i>
+							</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+</main>
+
+<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/footer.php"); ?>
+<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/docfoot_sub.php"); ?>
+
+</body>
+</html>
