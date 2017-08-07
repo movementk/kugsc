43d16f84ecad54075bc393d66b0f58be4c940af0
mk-kimjh <mk.kimjh@gmail.com>
2017-08-07 18:51:00 +0900

    about 완료

---
+++ about/contact_us.php
@@ -0,0 +1,235 @@
+<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/dochead_sub.php"); ?>
+<link href="/assets/css/about.css" rel="stylesheet">
+</head>
+<body class="about">
+
+<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/header.php"); ?>
+<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/side_nav.php"); ?>
+
+<main id="content" class="contact-us">
+
+	<!-- Page header -->
+	<?php require_once($_SERVER["DOCUMENT_ROOT"]."/about/_page_header.php"); ?>
+	<!-- // Page header -->
+	
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-lg-3">
+			
+				<!-- LNB -->
+				<?php require_once($_SERVER["DOCUMENT_ROOT"]."/about/_lnb.php"); ?>
+				<!-- // LNB -->
+				
+			</div>
+			<div class="col-lg-9">
+				
+				<div class="page-content">
+				
+					<!-- 페이지 타이틀 -->
+					<div class="page-title">
+						<h3>Contact Us</h3>
+					</div>
+                  
+                  	<div class="page-common">
+                   
+						<p class="main-building">
+							<img class="img-responsive visible-xs" src="/assets/images/about/contact/img_main_building_xs.jpg" alt="Korea University Main Building">
+							<img class="img-responsive visible-sm" src="/assets/images/about/contact/img_main_building_sm.jpg" alt="Korea University Main Building">
+							<img class="img-responsive visible-md visible-lg" src="/assets/images/about/contact/img_main_building_lg.jpg" alt="Korea University Main Building">
+						</p>
+
+						<h5 class="u-title">International students <br class="visible-xs">(Undergraduate Program)</h5>
+						<dl>
+							<dt class="tel">Tel</dt>
+							<dd class="tel">+82-2-3290-5173, 5174</dd>
+							<dt class="email">Email</dt>
+							<dd class="email"><a herf="mailto:gsc@korea.ac.kr" target="_blank">gsc@korea.ac.kr</a></dd>
+							<dt class="fax">Fax</dt>
+							<dd class="fax">+82-921-2352</dd>
+						</dl>
+
+						<h5 class="u-title add-mark">
+							<span><i></i>Undergraduate prospective student can contact the Office of International Affairs</span><br>
+							<span><i></i>International Admissions Team for admission related inquiries,</span>
+						</h5>
+						<dl>
+							<dt class="tel">Tel</dt>
+							<dd class="tel">+82-2-3290-5156,5157</dd>
+							<dt class="email">Email</dt>
+							<dd class="email"><a herf="mailto:admission@adm.korea.ac.kr" target="_blank">admission@adm.korea.ac.kr</a></dd>
+							<dt class="fax">Fax</dt>
+							<dd class="fax">+82-2-922-5820</dd>
+						</dl>
+
+						<h5 class="u-title">Exchange and Visiting students (to KU)</h5>
+						<dl>
+							<dt class="tel">Tel</dt>
+							<dd class="tel">+82-2-3290-5177, 5178</dd>
+							<dt class="email">Email</dt>
+							<dd class="email"><a herf="mailto:studyabroad@korea.ac.kr" target="_blank">studyabroad@korea.ac.kr</a></dd>
+							<dt class="fax">Fax</dt>
+							<dd class="fax">+82-921-2352</dd>
+						</dl>
+
+						<h5 class="u-title">Working hours</h5>
+						<p>
+							Monday - Friday (from 9:00am to 17:30pm)<br>
+							Closed on Saturday, Sunday and Public Holidays.
+						</p>
+
+						<h5 class="u-title">Location</h5>
+						<address>Central Plaza B1 #104A, 145 Anam-Ro, Seongbuk-Gu, Seoul 02841, Korea (near KU Pride Club)</address>
+
+						<h5 class="u-title">Direction to KU:</h5>
+						<p>
+							The Incheon International Airport (IIA), in the city of Incheon, is the largest and main international airport in Korea. It takes about 60 minutes to get to Korea University by car.
+						</p>
+						<figure>
+							<img class="img-responsive visible-xs" src="/assets/images/about/contact/img_route_xs.jpg">
+							<img class="img-responsive visible-sm" src="/assets/images/about/contact/img_route_sm.jpg">
+							<img class="img-responsive visible-md visible-lg" src="/assets/images/about/contact/img_route_lg.jpg">
+							<figcaption>
+								The quickest way to get to KU from IIA is to come to downtown Seoul first and then to transfer to subway there. Time may vary depending on traffic situation of the day, but it usually takes approximately one and a half hours.
+							</figcaption>
+						</figure>
+                   
+                   	</div>
+                   	
+                   	<div class="tabs dropdown">
+						<button class="btn btn-defualt btn-block" data-toggle="dropdown">
+							Select
+						</button>
+						<div class="dropdown-menu">
+							<ul class="nav nav-tabs nav-justified" role="tablist">
+								<li role="presentation" class="active"><a href="#about-us-1" aria-controls="about-us-1" role="tab" data-toggle="tab">By Airport Limousine</a></li>
+								<li role="presentation"><a href="#about-us-2" aria-controls="about-us-2" role="tab" data-toggle="tab">By Subway</a></li>
+								<li role="presentation"><a href="#about-us-3" aria-controls="about-us-3" role="tab" data-toggle="tab">By Taxi</a></li>
+								<li role="presentation"><a href="#about-us-4" aria-controls="about-us-4" role="tab" data-toggle="tab">Campus map</a></li>
+							</ul>
+						</div>
+					</div>
+
+					<!-- 탭 패널 -->
+					<div class="tab-content">
+						<div role="tabpanel" class="tab-pane" id="about-us-1">
+							<h4 class="title">By Airport Limousine</h4>
+							<p>
+								Dobong and Seongbuk bound bus (Bus #6101) is available at the bus stop near gate 3 and gate 9 (passenger terminal on the 1st floor at IIA).<br>
+								For more information such as fee, duration etc, please visit http://www.calt.co.kr -> click on English at the right top -> Click on Limousine Business -> Click on Routes and Bus stops -> Click on 6101 (Dobong, Seongbuk )
+							</p>
+							<div class="route-6100">
+								<h5 class="u-title">Route numer 6101</h5>
+								<p>
+									Dobong, Seong <i class="icon-resize-horizontal"></i> Gimpo Airport <i class="icon-resize-horizontal"></i> Incheon Airport<br>
+									(Via Seoul Ring Road)
+								</p>
+								<p class="route-graph">
+									<img class="img-responsive visible-xs" src="/assets/images/about/contact/img_route_bus_6100_xs.jpg" alt="">
+									<img class="img-responsive visible-sm" src="/assets/images/about/contact/img_route_bus_6100_sm.jpg" alt="">
+									<img class="img-responsive visible-md visible-lg" src="/assets/images/about/contact/img_route_bus_6100_lg.jpg" alt="">
+								</p>
+							</div>
+							<div class="route-info">
+							
+								<h5 class="u-title">Routes information</h5>
+								
+								<h6>Operation hours</h6>
+								<ul class="u-list">
+									<li>Surak Terminal : 04:25 ~ 20:30</li>
+									<li>Gimpo Airport : 05:50 ~ 23:00  /  Incheon Airport : 06:20 ~ 23:30</li>
+								</ul>
+								
+								<h6>Intervals</h6>
+								<ul class="u-list">
+									<li>15 ~ 25 minutes</li>
+								</ul>
+								
+								<h6>Time to destination</h6>
+								<ul class="u-list">
+									<li>Dobong, Seongbuk <i class="icon-resize-horizontal"></i> Gimpo Airport : 70 minutes</li>
+									<li>Dobong, Seongbuk <i class="icon-resize-horizontal"></i> Incheon Airport : 105 minutes</li>
+									<li>Gimpo Airport <i class="icon-resize-horizontal"></i> Incheon Airport : 35 minutes</li>
+								</ul>
+								
+								<h6>Fares</h6>
+								<ul class="u-list">
+									<li>Dobong, Seongbuk <i class="icon-resize-horizontal"></i> Gimpo Airport : 70 minutes  :  Adult 7,500won  /  Child 5,00won</li>
+									<li>Dobong, Seongbuk <i class="icon-resize-horizontal"></i> Incheon Airport : 105 minutes  :  Adult 15,000won  /  Trans Card 14,000won / Child 10,000won</li>
+									<li>Gimpo Airport <i class="icon-resize-horizontal"></i> Incheon Airport : 35 minutes  :  Adult 7,500won  /  Child 5,00won</li>
+								</ul>
+								
+								<p>If you carry heavy and bulky baggage, it would be more convenient for you to take a taxi to Korea University after getting off at Sungrye Elementary School stop, which costs you around 3,000 KRW. </p>
+								
+							</div>
+						</div>
+						<div role="tabpanel" class="tab-pane" id="about-us-2">
+							<h4 class="title">By Subway</h4>
+							<ol class="co-list">
+								<li>
+									From Incheon Int’l Airport to Korea University station (at Humanities and Social Sciences campus) Take the Airport train -> Transfer to Line 6 at Gongdeok -> Take the subway line 6 -> Get off at Korea University station and exit through Exit no.1 -> Turn right and you are already on the Humanities and Social Sciences campus.
+									<p class="figure">
+										<img class="img-responsive" src="/assets/images/about/contact/img_subway_route.jpg" alt="">
+									</p>
+									<p class="figure">
+										<img class="img-responsive" src="/assets/images/about/contact/img_subway_entrance.jpg" alt="">
+									</p>
+								</li>
+								<li>
+									From Incheon Int’l Airport to Anam station (near Natural Sciences and Engineering campus) Take the Airport train -> Transfer to Line 6 at Gongdeok -> Take the subway line 6 -> Get off at Anam station and exit through Exit no.4 -> Walk straight for 5 mins, you will get to the Natural Sciences and Engineering campus.
+								</li>
+							</ol>
+							<div class="how-to-reach">
+								<p>
+									<i>How to reach Global Services Center at Central Plaza (Humanities and Social Science campus) From Korea University station (Line 6) : Exit from Exit no.1 and walk for 10 mins From Anam station (Line 6) : Exit from Exit no.2 and walk for 5 mins</i>
+								</p>
+								<p class="small">
+									From Korea University station (Line 6) : Exit from Exit no.1 and walk for 10 mins
+								</p>
+								<p class="small">
+									From Anam station (Line 6) : Exit from Exit no.2 and walk for 5 mins 
+								</p>
+								<p class="figure">
+									<img class="img-responsive" src="/assets/images/about/contact/img_subway_minutes.jpg" alt="">
+								</p>
+							</div>
+						</div>
+						<div role="tabpanel" class="tab-pane" id="about-us-3">
+							<h4 class="title">By Taxi</h4>
+							<p>
+								Taxis are available on the 1st floor at IIA, known as arrival floor. Standard Seoul Taxi is available at platform #6C, and deluxe taxi is available at platform #7C. International taxi (for international passengers only) is available at platform #4C and you can make an online reservation in advance.<br>
+								(Online booking <a href="http://www.airport.kr/pa/ko/d/4/2/7/index.jsp" target="_blank">http://www.airport.kr/pa/ko/d/4/2/7/index.jsp</a> / by email <a href="matilto:reserve@intltaxi.co.kr" target="_blank">reserve@intltaxi.co.kr</a>)
+							</p>
+							<p>
+								Expected Fare: 55,000 KRW approximately (based on standard Seoul taxi)<br>
+								Expected Travel Time: 90 mins
+							</p>
+							<p class="figure">
+								<img class="img-responsive" src="/assets/images/about/contact/img_airport_gate.jpg" alt="">
+							</p>
+							<p>
+								Basic Fare : Standard taxi- 3,000 KRW / Deluxe taxi – 5,000 KRW
+							</p>
+							<p class="note">12:00am - 4:00am additional 20% late night premium applicable for standard taxi only</p>
+							<p class="note">Deluxe taxis are colored black and called “mo-bum taxi"</p>
+						</div>
+						<div role="tabpanel" class="tab-pane active" id="about-us-4">
+							<h4 class="title">Campus map</h4>
+							<div class="map">
+								구글지도 연동 영역(개발 후 확인 가능함)
+							</div>
+						</div>
+					</div>
+                    
+				</div>
+				
+			</div>
+		</div>
+	</div>
+	
+</main>
+
+<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/footer.php"); ?>
+<?php require_once($_SERVER["DOCUMENT_ROOT"]."/inc/docfoot_sub.php"); ?>
+
+</body>
+</html>
